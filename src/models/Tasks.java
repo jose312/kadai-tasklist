28b1cc9867455fd19e534b54b3a0cb63e066736e
jose <kfy081031@gmail.com>
2018-04-26 10:42:35 +0900

    Add database filea

---
+++ src/models/Tasks.java
@@ -0,0 +1,107 @@
+package models;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tasks")
+public class Tasks{
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name="title",length=255,nullable=false)
+	private String title;
+
+	@Column(name="content",length=255,nullable=false)
+	private String content;
+
+	@Column(name="content_2",length=255)
+	private String content_2;
+
+	@Column(name="startdate",nullable=false)
+	private Date sturtdate;
+
+	@Column(name="finishdate",nullable=false)
+	private Date finishdate;
+
+	@Column(name="created_at",nullable=false)
+	private Timestamp created_at;
+
+	@Column(name="updated_at",nullable=false)
+	private Timestamp updated_at;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getContent_2() {
+		return content_2;
+	}
+
+	public void setContent_2(String content_2) {
+		this.content_2 = content_2;
+	}
+
+	public Date getSturtdate() {
+		return sturtdate;
+	}
+
+	public void setSturtdate(Date sturtdate) {
+		this.sturtdate = sturtdate;
+	}
+
+	public Date getFinishdate() {
+		return finishdate;
+	}
+
+	public void setFinishdate(Date finishdate) {
+		this.finishdate = finishdate;
+	}
+
+	public Timestamp getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Timestamp created_at) {
+		this.created_at = created_at;
+	}
+
+	public Timestamp getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(Timestamp updated_at) {
+		this.updated_at = updated_at;
+	}
+
+
+}
