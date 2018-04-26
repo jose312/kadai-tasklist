86c1eb91ed5d7efcbaa394be812b8256ca882bf8
jose <kfy081031@gmail.com>
2018-04-26 11:59:58 +0900

    e

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -8,14 +8,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name="tasks")
 public class Tasks{
 	@Id
 	@Column(name="id")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	@Column(name="title",length=255,nullable=false)
